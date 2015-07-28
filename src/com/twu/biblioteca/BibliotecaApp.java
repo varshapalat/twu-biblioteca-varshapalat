3190f23f933ed05414f2ffc91ec724ad0417f88c
varshapalat <varshapalat@gmail.com>
2015-07-28 12:30:25 +0530

    #0001| Added welcome message for customer

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        WelcomeMessage welcomeMessage = new WelcomeMessage();
+        welcomeMessage.print();
     }
 }
---
+++ src/com/twu/biblioteca/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+public class WelcomeMessage {
+    public void print() {
+        System.out.print("Welcome!! \nBiblioteca is now available");
+    }
+}
