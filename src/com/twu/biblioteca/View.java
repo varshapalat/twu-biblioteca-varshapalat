124f089bb4486eee569d9db36481498d3403d0cb
varshapalat <varshapalat@gmail.com>
2015-07-28 18:23:00 +0530

    #0002| Added the welcome message printing feature

---
+++ src/com/twu/biblioteca/View.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class View {
+    String welcomeMessage = "Welcome!! \nBiblioteca is now available";
+    public void printWelcomeMessage() {
+        System.out.print(welcomeMessage);
+    }
+}
