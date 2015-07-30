c69bf06f23f63ffb1bcdd57758ea0fffcf7d153f
varshapalat <varshapalat@gmail.com>
2015-07-30 18:03:15 +0530

    #0018| Added functionality to library so that customer can return a book

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -4,25 +4,38 @@ import java.util.ArrayList;
 
 public class Library {
     ArrayList<ArrayList<String>> listOfAllLibraryBooks;
-    boolean checkout;
+    ArrayList<ArrayList<String>> listOfCheckedOutLibraryBooks = new ArrayList<ArrayList<String>>();
 
     public Library(ArrayList<ArrayList<String>> books) {
         listOfAllLibraryBooks = books;
     }
 
-    public boolean checkout(String s) {
-        checkout = false;
+    public boolean checkout(String nameOfBookToCheckout) {
+        boolean checkout = false;
         ArrayList<String> book = new ArrayList<String>();
 
         for (int i = 0; i < listOfAllLibraryBooks.size(); i++) {
-            if (s.equals(listOfAllLibraryBooks.get(i).get(0))) {
+            if (nameOfBookToCheckout.equals(listOfAllLibraryBooks.get(i).get(0))) {
                 checkout = true;
                 for (int j = 0; j < listOfAllLibraryBooks.get(0).size(); j++) {
                     book.add(listOfAllLibraryBooks.get(i).get(j));
                 }
+                listOfCheckedOutLibraryBooks.add(book);
             }
         }
         listOfAllLibraryBooks.remove(book);
         return checkout;
+    }
+
+    public void returnABook(String nameOfBookToReturn) {
+        ArrayList<String> book = new ArrayList<String>();
+
+        for (int i = 0; i < listOfCheckedOutLibraryBooks.size(); i++) {
+            if(nameOfBookToReturn.equals(listOfCheckedOutLibraryBooks.get(i).get(0))) {
+                book = listOfCheckedOutLibraryBooks.get(i);
+                listOfCheckedOutLibraryBooks.remove(book);
+            }
+        }
+        listOfAllLibraryBooks.add(book);
     }
 }
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -12,7 +12,6 @@ public class LibraryTest {
     @Test
     public void libraryCanCheckOutABook() {
         ArrayList<ArrayList<String>> expectedListOfBooks = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<String>> actualListOfBooks;
         ArrayList<ArrayList<String>> listOfAllLibraryBooks = new ArrayList<ArrayList<String>>();
 
         ArrayList<String> bookOne = new ArrayList<String>();
@@ -36,6 +35,35 @@ public class LibraryTest {
         Library library = new Library(listOfAllLibraryBooks);
 
         library.checkout("Kane and Abel");
+
+        assertEquals(expectedListOfBooks, listOfAllLibraryBooks);
+    }
+    @Test
+    public void libraryCanReturnABook() {
+        ArrayList<ArrayList<String>> expectedListOfBooks = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> listOfAllLibraryBooks = new ArrayList<ArrayList<String>>();
+
+        ArrayList<String> bookOne = new ArrayList<String>();
+        bookOne.add("The Da Vinci Code");
+        bookOne.add("Dan Brown");
+        bookOne.add("2000");
+        ArrayList<String> bookTwo = new ArrayList<String>();
+        bookTwo.add("Kane and Abel");
+        bookTwo.add("Jeffrey Archer");
+        bookTwo.add("1979");
+        ArrayList<String> bookThree = new ArrayList<String>();
+        bookThree.add("The Alchemist");
+        bookThree.add("Paulo Coelho");
+        bookThree.add("1988");
+        listOfAllLibraryBooks.add(bookOne);
+        listOfAllLibraryBooks.add(bookThree);
+        expectedListOfBooks.add(bookOne);
+        expectedListOfBooks.add(bookThree);
+        expectedListOfBooks.add(bookTwo);
+        Library library = new Library(listOfAllLibraryBooks);
+        library.listOfCheckedOutLibraryBooks.add(bookTwo);
+
+        library.returnABook("Kane and Abel");
 
         assertEquals(expectedListOfBooks, listOfAllLibraryBooks);
     }
